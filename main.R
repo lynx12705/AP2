2f2f62657c75236a455958d726c12de9ed7125f0
Io <lynx12705@gmail.com>
2026-05-04 21:50:28 +0100

    ok

--- main.R
+++ main.R
@@ -1,2 +1,23 @@
-Hey guys
-git ad
+#function for R0
+R0 <- function(beta, gamma, N){
+  return(beta * N / gamma)
+}
+
+# Parameters
+beta <- 0.02
+N <- 100
+
+# Range of gamma
+gamma_vals <- seq(0.2, 4, 0.01)
+
+# Compute R0
+R0_vals <- R0(beta, gamma_vals, N)
+
+# Plot
+plot(gamma_vals, R0_vals, type="l",
+     xlab="Recovery rate (gamma)",
+     ylab="R0",
+     main="R0 vs Recovery Rate")
+
+# Add threshold line
+abline(h=1, col="red", lty=2)
